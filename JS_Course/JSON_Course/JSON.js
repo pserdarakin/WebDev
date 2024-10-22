b2e3df5d348859c81718c652732e716aa1842461
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-10-22 18:32:44 +0100

    first commit for json

---
+++ JS_Course/JSON_Course/JSON.js
@@ -0,0 +1,20 @@
+/* JavaScript Object Notation 
+- Lightweight data-interchange format, and use to send data back and forward to the server as text.
+
+JSON Syntax Rules
+- Uses key/value pairs - {"name":"Brad"}
+- Double quotes
+- .json
+- MIME type is "Application/json"
+
+Example
+
+{
+    "name":"Serdar Akin",
+    "age":28,
+    "address":{
+        "street":"5 main st",
+        "city":"Boston"
+    },
+    "children":["Brianna","Nicholas"]
+}
