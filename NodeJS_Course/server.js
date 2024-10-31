77318ebdecc587704de2a93eb6cea9ad579d157f
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-10-31 04:55:25 +0000

    commit loading files section

--- NodeJS_Course/server.js
+++ NodeJS_Course/server.js
@@ -1,5 +1,13 @@
 import http from 'http';
+import fs from 'fs/promises'; // there are version sncrnyos, insncrnyos and promise versions
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 /* 
 23:13 - ES Modules
@@ -32,7 +40,7 @@ server.listen(PORT, () => {
 
 */
 
-// Simple Routing
+/* Simple Routing
 
 const server = http.createServer((req, res) => {
     try {
@@ -50,6 +58,41 @@ const server = http.createServer((req, res) => {
                 res.end('<h1>Not Found!</h1>');
             }
 
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(404, { 'Content-type': 'text/plain' });
+        res.end('Server Error');
+    }
+
+    }
+);
+
+// Just above will not do anything by itself, so we have to listen on port
+server.listen(PORT, () => {
+    // pass on some function so that it can do smt it connects
+    console.log(`Server running on port; ${PORT}`);
+});
+*/
+
+// Loading files (created public folder, and imported fs url and path and get current path part)
+const server = http.createServer(async(req, res) => {
+    try {
+        // Check if GET request
+        if(req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if(req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+               throw new Error('Not Found');
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method not allowed');
         }
