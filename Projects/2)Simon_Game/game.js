a38adc1f1e813a518e95707a3312482ffa7676e0
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-11-20 15:56:36 +0000

    animatePress and playSound functionality added

--- Projects/2)Simon_Game/game.js
+++ Projects/2)Simon_Game/game.js
@@ -2,9 +2,16 @@ const gamePattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
+const userClickedPattern = [];
+
 // when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -12,30 +19,21 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    // Animate a flash to the selected button
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    // Javascript to play the sound for the button colour selected
-    var sounds = new Audio("sounds/" + randomChosenColour + ".mp3");
-    sounds.play();
 
-
-    // new var for store the id of the button that got clicked
-    
- 
+    playSound(randomChosenColour);
 };
 
+function playSound(name) {
+    // Javascript to play the sound for the button colour selected
+    var sounds = new Audio("sounds/" + name + ".mp3");
+    sounds.play();  
+};
 
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed")
+    }, 100)
+};
 
-/* Step 4 - Check Which Button is Pressed
-1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
-
-2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-
---> So if the Green button was clicked, userChosenColour will equal its id which is "green".
-
-
-3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
-
-4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-
-At this stage, if you log the userClickedPattern you should be able to build up an array in the console by clicking on different buttons. */
