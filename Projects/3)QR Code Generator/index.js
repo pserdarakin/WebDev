33dd2dad0f38fe5cee372968ebfa1168a9bb0dd0
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-12-05 12:34:21 +0000

    first commit of QR code generator

--- Projects/3)QR Code Generator/index.js
+++ Projects/3)QR Code Generator/index.js
@@ -4,3 +4,24 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
+import { default as inquirer } from "inquirer";
+import { image } from "qr-image";
+
+const questions = [
+    {
+        type: 'input',
+        name: 'website',
+        message: 'Type the address that you want to convert to QR',
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, ''));
+});
+
+var qr = require('qr-image');
+
+var qr_img = qr.image(answers.website, {type: 'img'});
+qr_img.pipe(require('fs').createWriteStream('lets_go.img'));
+
+var img_string = qr.imageSync(answers.website, {type: 'img'});
