28efd0be6a22920ee6f5ea20351e5e5fb105e824
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-12-14 16:17:00 +0000

    feat: add data passing functionality between server and EJS templates
    - Implement form submission to calculate the number of letters in a name
    - Use `res.render` to pass data (`numberOfLetters`) from the server to the EJS view
    - Update `index.ejs` to conditionally display results or the input form
    - Add form with fields for first and last name

--- NodeJS_Course/EJS/index.js
+++ NodeJS_Course/EJS/index.js
@@ -6,3 +6,14 @@
 <%# This is a comment %>  // Stop Execution
 <%- include("header.js") %> // Insert another EJS file, instead of writing header and footer each time you can just implement 
 */
+
+/* EJS & Passing Data: Passing data from server to client and client to server
+reminder; Server -> EJS, we were using res.render("index.ejs") and any sort of data to be rendered, data has property value which can be picked up by ejs tags inside the .ejs file
+format; res.render("index.ejs", data)
+-What if there is no Data? => ejs not workins as traditional programming language mentality by mean that is it is not going to look for the variable instead it will try to use directly and the outcome will be just crash.
+-What can be done => inside the .ejs file `locals` property can be use to assure to access all the rendered .js file
+
+EJS -> Server (to store it or smth)
+form -> server 
+
+ */
