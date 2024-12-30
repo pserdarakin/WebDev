2ba59eaaf94e4d4731f24436f1e17b41a262b8f2
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2024-12-30 15:14:23 +0000

    feat: implement post creation and display functionality in the blog

--- Projects/My Blog - Capstone Project/index.js
+++ Projects/My Blog - Capstone Project/index.js
@@ -3,12 +3,21 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: posts});
+});
+
+app.post("/submit", (req, res) => {
+    const newPost = req.body.postContent;
+    posts.push(newPost);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
