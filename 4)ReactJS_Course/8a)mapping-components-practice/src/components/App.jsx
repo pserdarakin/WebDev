9472ca4cb539302e084cde32a6c001484126f065
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2025-01-31 19:19:06 +0000

    feat: exercise and hand-on practices for mapping componenets

---
+++ 4)ReactJS_Course/8a)mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emoji){
+  return <Entry
+  key={emoji.id}
+  emoji={emoji.emoji}
+  name={emoji.name}
+  meaning={emoji.meaning}
+  />;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+      {emojipedia.map(createEntry)}
+      </dl>
+    </div>
+  );
+}
+
+// feat: exercise and hand-on practices for mapping componenets
+
+export default App;
