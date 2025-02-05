f1ab677c4e698d3f56f662acdea08ea39ab93abf
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2025-02-05 18:47:43 +0000

    practice on es6 destructring

--- 4)ReactJS_Course/13)es6-destructuring/src/index.js
+++ 4)ReactJS_Course/13)es6-destructuring/src/index.js
@@ -1,7 +1,15 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
 
+const [honda, tesla] = cars;
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
 
 
 ReactDOM.render(
